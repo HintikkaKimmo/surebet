a8f9d288d4e7ebce45e5fe68e36e82b77b3c06ad
Kimmo Hintikka <hintikkakimmo@gmail.com>
2018-04-05 21:31:00 +0100

    deleted click cli tests moving whole project to using starndard agparse due to Unicode errors with click + Python3

--- surebet/cli.py
+++ surebet/cli.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Console script for surebet."""
-import sys
-import click
-
-
-@click.command()
-def main(args=None):
-    """Console script for surebet."""
-    click.echo("Replace this message by putting your code into "
-               "surebet.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+import argparse
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+
+    parser = argparse.ArgumentParser()
+
+    # define each option with: parser.add_argument
+
+    args = parser.parse_args()  # automatically looks at sys.argv
+
+    # access results with: args.argumentName
--- tests/test_surebet.py
+++ tests/test_surebet.py
@@ -5,11 +5,6 @@
 
 import pytest
 
-from click.testing import CliRunner
-
-# from surebet import surebet
-from surebet import cli
-
 
 @pytest.fixture
 def response():
@@ -29,10 +24,3 @@ def test_content(response):
 
 def test_command_line_interface():
     """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'surebet.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
